9f344f449de68d326c6d0bcac0febacdb109bec0
Alexis Montes <montes.alexis25@gmail.com>
2021-02-16 23:54:31 -0500

    cleaned up logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import react, { useEffect } from 'react';
 import useCountDown from 'react-countdown-hook';
 
+let formatTime = (timeInSec) => {
+  return new Date(timeInSec * 1000).toISOString().substr(14, 5)
+
+};
+
+
 function App() {
   // set default time values in state variables
   const [workTimer, setWorkTimer] = react.useState(25*60);
@@ -18,14 +24,14 @@ function App() {
   let handlePlayClick = () => {
     if(timeLeft === 0){
       setWorkTimerRunning(!workTimerRunning)
-      setBreakTimerRunning(!breakTimerRunning)
+      setBreakTimerRunning(breakTimerRunning)
 
-      if(workTimerRunning){
-        start(workTimer * 1000)
+      if(!workTimerRunning){
+        start(breakTime  * 1000)
         setActualTimerRunning(true)
       }
       else {
-        start(breakTime * 1000)
+        start(workTimer * 1000)
         setActualTimerRunning(true)
       }
     }
@@ -134,20 +140,27 @@ function App() {
           className="col-3"
           onClick={handlePlayClick}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" className="bi bi-stop-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z"/>
-          </svg>
+          {
+            actualTimerRunning ?
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" className="bi bi-stop-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z"/>
+            </svg>
+            :
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" className="bi bi-play-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
+            </svg>
+            
+          }
+          
+          
         </div>
         <div id="session-left" className="col-3">
           <p
             className="h3"
           >
-            {(timeLeft)}
+            { Math.floor((timeLeft/1000)/60) + ":" + (timeLeft/1000)%60 }
           </p>
         </div>
         <div id="reset" className="col-3">
